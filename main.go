a4a67753fb263dc0b1e29a2ad7a7d5cfe7300c73
Ghaisan Daffa <ghaisanfayadh@upi.edu>
2025-09-26 18:23:02 +0700

    bisa local jaringan

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ package main
 import (
 	"context"
 	"log"
+	"net"
+	"os"
 
 	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go/v4"
@@ -40,6 +42,18 @@ func setupFirebase() (*auth.Client, *firestore.Client) {
 	return authClient, firestoreClient
 }
 
+// getLocalIP returns the local IP address of the machine
+func getLocalIP() string {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		return "localhost"
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	return localAddr.IP.String()
+}
+
 func main() {
 	authClient, firestoreClient := setupFirebase()
 	// Jangan lupa menutup koneksi firestore saat aplikasi berhenti
@@ -49,6 +63,26 @@ func main() {
 
 	routes.SetupRoutes(router, authClient, firestoreClient)
 
-	log.Println("Server running on http://localhost:3000")
-	router.Run(":3000")
+	// Konfigurasi host dan port
+	host := os.Getenv("HOST")
+	if host == "" {
+		host = "0.0.0.0" // Default: listen di semua interface jaringan (localhost + jaringan lokal)
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000" // Default port
+	}
+
+	address := host + ":" + port
+
+	log.Printf("Server running on http://%s", address)
+	if host == "0.0.0.0" {
+		log.Printf("Local network access: http://%s:%s", getLocalIP(), port)
+		log.Printf("Localhost access: http://localhost:%s", port)
+	} else if host == "localhost" || host == "127.0.0.1" {
+		log.Printf("Localhost access only: http://localhost:%s", port)
+	}
+
+	router.Run(address)
 }
